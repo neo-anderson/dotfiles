145f2dd789e952fe2eee78e126c407a01ff5f7a3
Aswin <hi.aswin@gmail.com>
2021-01-12 14:48:50 -0800

    Adding config.fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,8 @@
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /Users/aswin/miniconda/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+
+# Keeping startup configuration in ~/.bashrc to share it with other shells
+bass source ~/.bashrc
+
